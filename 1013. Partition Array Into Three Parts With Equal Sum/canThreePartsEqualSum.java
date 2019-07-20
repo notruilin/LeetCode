84d7cd655a37d91cb18b7b408a34a32e6b48199b
Ruilin Liu <notruilin@gmail.com>
2019-07-20 21:59:02 +1000

    Update canThreePartsEqualSum.java

--- 1013. Partition Array Into Three Parts With Equal Sum/canThreePartsEqualSum.java
+++ 1013. Partition Array Into Three Parts With Equal Sum/canThreePartsEqualSum.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canThreePartsEqualSum(int[] A) {
-        if (A.length < 3)   return false;
         for (int i = 1; i < A.length; i++) {
             A[i] += A[i-1];
         }
+        if (A[A.length - 1] % 3 != 0)   return false;
         int target = A[A.length - 1] / 3;
         int i = -1, j = -1;
         for (int k = 0; k < A.length - 1; k++) {
@@ -12,14 +12,12 @@ class Solution {
                 break;
             }
         }
-        if (i == -1)    return false;
         for (int k = A.length - 2; k > i; k--) {
             if (A[k] == target * 2) {
                 j = k;
                 break;
             }
         }
-        if (j == -1)    return false;
         if (i < j)  return true;
         return false;
     }
