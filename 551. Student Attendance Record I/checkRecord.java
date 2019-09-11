712f44be079d09e20bb974124063ae7f13fffa5d
Ruilin Liu <notruilin@gmail.com>
2019-09-11 22:17:56 +1000

    Update checkRecord.java

--- 551. Student Attendance Record I/checkRecord.java
+++ 551. Student Attendance Record I/checkRecord.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkRecord(String s) {
-        String newS = s.replaceAll("A", "");
+        String newS = s.replace("A", "");
         if (s.length() - newS.length() > 1) return false;
         if (s.contains("LLL"))   return false;
         return true;
