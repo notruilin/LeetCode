9fc70879750b91a455f2aa6263e03717ca90621a
Ruilin Liu <notruilin@gmail.com>
2019-09-26 19:09:49 +1000

    Create searchInsert.java

---
+++ 35. Search Insert Position/searchInsert.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right)/2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
