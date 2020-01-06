dd7ace227e9a5647e681cf16f44831d251150b7c
Ruilin Liu <notruilin@gmail.com>
2020-01-06 13:45:19 +1100

    Create xorQueries.java

---
+++ 1310. XOR Queries of a Subarray/xorQueries.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] xor = new int[arr.length];
+        xor[0] = arr[0];
+        for (int i = 1; i < arr.length; i++)
+            xor[i] = xor[i-1] ^ arr[i];
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            if (queries[i][0] > 0)
+                ans[i] = xor[queries[i][1]] ^ xor[queries[i][0] - 1];
+            else
+                ans[i] = xor[queries[i][1]];
+        }
+        return ans;
+    }
+}
