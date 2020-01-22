36b0b87c7861b36d4b6f93582807787e2da4bd46
Ruilin Liu <notruilin@gmail.com>
2020-01-22 19:55:27 +1100

    Create smallestRepunitDivByK.java

---
+++ 1015. Smallest Integer Divisible by K/smallestRepunitDivByK.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestRepunitDivByK(int K) {
+        if (K % 2 == 0 || K % 5 == 0)   return -1;
+        int count = 1, N = 1;
+        while (N % K != 0) {
+            N = (N % K) * 10 + 1;
+            count++;
+        }
+        return count;
+    }
+}
